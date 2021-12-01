5989ba24a2b10f3ab5234a1ce2c806a8e8085c3b
ahmedzg1 <89781515+ahmedzg1@users.noreply.github.com>
2021-12-01 15:17:27 +0100

    Add files via upload

---
+++ mymodule.js
@@ -0,0 +1,14 @@
+module.exports = function (dir, filterStr, callback) {
+
+    fs.readdir(dir, function (err, list) {
+        if (err) {
+            return callback(err);
+        }
+
+        list = list.filter(function (file) {
+            return path.extname(file) === '.' + filterStr
+        });
+
+        callback(null, list)
+    })
+}
